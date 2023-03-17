6d28a5380fe47a4722289b4c8ff1fd1ba169a749
Foster <fosterunlimited0123@gmail.com>
2023-03-17 13:08:40 +0530

    Authly Email Sending Feature Implemented

--- src/app/controllers/forgotPasswordController.js
+++ src/app/controllers/forgotPasswordController.js
@@ -4,54 +4,59 @@ const { google } = require('googleapis')
 
 const OAuth2 = google.auth.OAuth2;
 
-// const OAuthClient = new OAuth2({
-// 	clientId:process.env.OAUTH_CLIENT_ID,
-// 	clientSecret: process.env.OAUTH_CLIENT_SECRET,
-// 	redirectUri: process.env.OAUTH_REDIRECT_URI
-// })
-
-// OAuthClient.setCredentials({
-// 	refresh_token: process.env.OAUTH_REFRESH_TOKEN
-// })
-// const OAuthAccessToken = OAuthClient.getAccessToken()
 const sendForgotPasswordPage = (req, res) => {
 
 	res.render('pages/forgot-password.pug')
 }
 
+const createTransporter = async () => {
+	const OAuthClient = new OAuth2({
+		clientId: process.env.OAUTH_CLIENT_ID,
+		clientSecret: process.env.OAUTH_CLIENT_SECRET,
+		redirectUri: process.env.OAUTH_REDIRECT_URI
+	})
+
+	OAuthClient.setCredentials({
+		refresh_token: process.env.OAUTH_REFRESH_TOKEN
+	})
+	const OAuthAccessToken = await OAuthClient.getAccessToken()
+	let transporter = nodeMailer.createTransport({
+		service: 'Gmail',
+		host: process.env.NODMAILER_HOST,
+		port: process.env.NODEMAILER_PORT,
+		secure: true,
+		auth: {
+			type: "OAuth2",
+			user: process.env.NODEMAILER_EMAIL,
+			clientId: process.env.OAUTH_CLIENT_ID,
+			clientSecret: process.env.OAUTH_CLIENT_SECRET,
+			refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+			accessToken: OAuthAccessToken
+		}
+	})
+	return transporter;
+}
 const sendPasswordResetURL = async (req, res) => {
+	let user = await req.body.password_reset_email
 	const html5 = 
-	`
+		`
 		<h1>Password Reset</h1>
 		<p>Authly</p>
 		<a href="#">link</a>
 	`
-	let transporter = nodeMailer.createTransport({
-		service: 'Gmail',
-		host: process.env.NODEMAILER_EMAIL,
-		port: process.env.NODEMAILER_PORT,
-		secure: true,
-		auth: {
-			type: "oauth2",
-			clientId:process.env.OAUTH_CLIENT_ID,
-			clientSecret: process.env.OAUTH_CLIENT_SECRET,
-			refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-			accessToken:await OAuthAccessToken
-		}
-	})
-	const msg = transporter.sendMail({
+	const emailTransporter = await createTransporter()
+	emailTransporter.sendMail({
 		from: `Foster Z (${process.env.NODEMAILER_EMAIL})`,
-		to: `You (${req.body.password_reset_email})`,
+		to: `${user}`,
 		subject: "Reset Password",
 		html: html5
 	}, (err, data) => {
-		if(err) console.error(err)
+		if (err) console.error(err)
 		else {
 			console.log(`Message sent: ${data.messageId}`)
 		}
-	})
-	console.log(OAuthAccessToken)
-	transporter.close()
+	}) 
+	emailTransporter.close()
 	res.redirect("/forgot-password")
 }
 
--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -5,10 +5,10 @@ const client = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     port: process.env.DB_PORT,
 })
-const connectionString = process.env.DB_STRING;
+const connectionString = process.env.DB_CONN;
 // const client = new Pool({connectionString})
 
 module.exports = { client }
